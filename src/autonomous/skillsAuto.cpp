5ccf13616cb4e7a1be9e421c916d771a1eb839a0
Toussaint24 <jaydenr.toussaint@gmail.com>
2025-03-09 21:18:19 -0500

    Optimized to reduce runtime and improve consistency

--- src/autonomous/skillsAuto.cpp
+++ src/autonomous/skillsAuto.cpp
@@ -8,65 +8,65 @@ void skillsAuto() {
     // putting first ring on wall stake
     chassis->setMaxVelocity(0.7*chassis->getMaxVelocity());
     gyro.set_heading(0);
-    setDriveMotorCurrentLimits(1400);
+    //setDriveMotorCurrentLimits(1400);
     setLadybrownPosition(200, 50);
     pros::delay(500);
     setLadybrownPosition(140, 50);
     pros::delay(500);
     setIntakeSpeed(127);
     setLiftSpeed(127);
-    chassis->moveDistance(15_in);
+    chassis->moveDistance(17_in);
     chassis->waitUntilSettled();
     chassis->moveDistance(-1.5_in);
     chassis->waitUntilSettled();
     pros::delay(300);
-    turnToHeading(90);
-    turnToHeading(180);
+    turnToHeading(183, 0.6);
     setLiftSpeed(0);
     setIntakeSpeed(0);
+    pros::delay(300);
     setLadybrownPosition(950, 150);
     pros::delay(1000);
-    chassis->moveDistance(-1_ft);
+    chassis->moveDistance(-1.1_ft);
     chassis->waitUntilSettled();
     setLadybrownPosition(0, 50);
 
     // going for next ring
-    turnToHeading(315);
+    turnToHeading(315, 0.7);
     setIntakeSpeed(127);
     pros::Task* intakeTask = new pros::Task(intakeUntilColor);
     chassis->moveDistance(5.5_ft);
     chassis->waitUntilSettled();
 
     // get ring from mid
-    turnToHeading(45);
+    turnToHeading(45, 0.7);
     chassis->moveDistance(3.6_ft);
     chassis->waitUntilSettled();
-    chassis->moveDistance(-3.4_ft);
+    chassis->moveDistance(-3.7_ft);
     chassis->waitUntilSettled();
     setIntakeSpeed(0);
     
     //getting mogo
-    turnToHeading(90);
-    chassis->moveDistance(-3.5_ft);
+    turnToHeading(90, 0.7);
+    chassis->moveDistance(-3.4_ft);
     chassis->waitUntilSettled();
     toggleMogoClamp();
     setIntakeSpeed(127);
     setLiftSpeed(127);
 
     // getting the ring by corner
-    turnToHeading(180);
-    chassis->moveDistance(3.7_ft);
+    turnToHeading(180, 0.6);
+    chassis->moveDistance(4.1_ft);
     chassis->waitUntilSettled();
 
     // going to the corner (time out)
-    turnToHeading(225);
+    turnToHeading(225, 0.7);
     chassis->moveDistanceAsync(2.6_ft);
     pros::delay(2000);
     chassis->moveDistance(-19_in);
     chassis->waitUntilSettled();
     setLiftSpeed(0);
     setIntakeSpeed(0);
-    turnToHeading(45);
+    turnToHeading(45, 0.6);
     chassis->moveDistance(-10_in);
     chassis->waitUntilSettled();
     toggleMogoClamp();
@@ -75,9 +75,7 @@ void skillsAuto() {
     // Drive to other side of field
     chassis->moveDistance(1_ft);
     chassis->waitUntilSettled();
-    chassis->moveDistance(0.5_ft);
-    chassis->waitUntilSettled();
-    turnToHeading(0);
+    turnToHeading(0, 0.7);
     setIntakeSpeed(127);
     pros::Task* intakeTaskTwo = new pros::Task(intakeUntilColor);
     chassis->setMaxVelocity(3*chassis->getMaxVelocity());
@@ -94,9 +92,9 @@ void skillsAuto() {
         {3.0, 0.00, 0},
         {0.0, 0, 0.0000}
     );
-    chassis->moveDistance(3.8_ft);
+    chassis->moveDistance(4.2_ft);
     chassis->waitUntilSettled();
-    turnToHeading(270);
+    turnToHeading(270, 0.7);
     chassis->moveDistance(-4.2_ft);
     chassis->waitUntilSettled();
     toggleMogoClamp();
