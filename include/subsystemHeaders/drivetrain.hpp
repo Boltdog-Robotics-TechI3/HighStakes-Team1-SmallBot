d1c5e053fda57d27bee2b15a5d39c6bd532901af
Toussaint24 <jaydenr.toussaint@gmail.com>
2025-03-14 06:09:57 -0500

    Added a PID struct to hold PID variables and an enum to define turn behavior for turnToHeading

--- include/subsystemHeaders/drivetrain.hpp
+++ include/subsystemHeaders/drivetrain.hpp
@@ -4,6 +4,22 @@ using namespace okapi;
 extern std::shared_ptr<okapi::ChassisControllerPID> chassis;
 
 extern std::shared_ptr<okapi::ChassisModel> drivetrain;
+struct PID {
+    double kP;
+    double kI;
+    double kD;
+    double smallErrorRange;
+    double smallErrorTimeout;
+    double largeErrorRange;
+    double largeErrorTimeout;
+    double minVelocity;
+};
+
+enum class TurnBehavior {
+    DEFAULT,
+    LEFT,
+    RIGHT
+};
 
 void drivetrainInitialize();
 
@@ -13,12 +29,8 @@ void arcadeDrive(bool reverse);
 
 void setDriveMotorCurrentLimits(int mAmps);
 
-void turnToHeading(double heading, double maxVelocity=1.0, int timeout=5);
+void turnToHeading(double heading, double maxVelocity=1.0, int timeout=5000, enum TurnBehavior behavior=TurnBehavior::DEFAULT);
 
-void turnAngle(double angle, double maxVelocity=1.0, int timeout=5);
+void turnAngle(double angle, double maxVelocity=1.0, int timeout=5000);
 
 void moveDistanceWithTimeout(QLength itarget, int timeout);
-
-void lateralPIDTune();
-
-void angularPIDTune();
--- src/subsystems/drivetrain.cpp
+++ src/subsystems/drivetrain.cpp
@@ -1,7 +1,4 @@
 #include "main.h"
-#include "okapi/api/chassis/controller/chassisControllerPid.hpp"
-#include "okapi/api/device/motor/abstractMotor.hpp"
-#include <string>
 using namespace std;
 using namespace okapi;
 
@@ -11,13 +8,25 @@ std::shared_ptr<okapi::ChassisControllerPID> chassis = std::dynamic_pointer_cast
 	.withMotors(leftMotorGroup, rightMotorGroup)
 	.withDimensions({AbstractMotor::gearset::blue, (3.0/4.0)}, {{2.75_in, 11.5_in}, imev5BlueTPR})
     .withGains(
-        {0.004, 0, 0.0001},
+        {0.00045, 0.0005, 0.00000},
         {2.5, 0, 0},
         {0.0, 0, 0.0000}
     )
     .build());
 // Turn gains: 3.0, 0.00, 1.5
 std::shared_ptr<ChassisModel> drivetrain = chassis->getModel();
+
+// PID constants for turning
+PID turnPID = {
+    .kP = 2.5,
+    .kI = 0.0,
+    .kD = 0.0,
+    .smallErrorRange = 0.4, // degrees
+    .smallErrorTimeout = 100.0, // milliseconds
+    .largeErrorRange = 0.75, // degrees
+    .largeErrorTimeout = 500.0, // milliseconds
+    .minVelocity = 3.0
+};
 
 /**
 *  Runs once when the codebase is initialized. 
@@ -26,7 +35,7 @@ std::shared_ptr<ChassisModel> drivetrain = chassis->getModel();
 void drivetrainInitialize() {
     //driverController.set_text(0, 0, "");
     drivetrain->setBrakeMode(okapi::AbstractMotor::brakeMode::coast);
-    chassis->setMaxVelocity(0.4*chassis->getMaxVelocity());
+    //chassis->setMaxVelocity(0.4*chassis->getMaxVelocity());
     setDriveMotorCurrentLimits(2000);
 }
 
@@ -39,24 +48,11 @@ void drivetrainInitialize() {
 *  will not work unless the robot is in manual control mode.
 */
 void drivetrainPeriodic() {
-    if (driverController.get_digital_new_press(DIGITAL_L2)) {
+    /*if (driverController.get_digital_new_press(DIGITAL_L2)) {
         isFrontReversed = !isFrontReversed;
-    }
+    }*/
 
     arcadeDrive(isFrontReversed);
-
-    // if (driverController.get_digital_new_press(DIGITAL_RIGHT)) {
-    //     turnToHeading(90);
-    // }
-    // if (driverController.get_digital_new_press(DIGITAL_LEFT)) {
-    //     turnToHeading(270);
-    // }
-    // if (driverController.get_digital_new_press(DIGITAL_UP)) {
-    //     turnToHeading(0);
-    // }
-    // if (driverController.get_digital_new_press(DIGITAL_DOWN)) {
-    //     turnToHeading(180);
-    // }
 }
 
 /**
@@ -91,8 +87,7 @@ void arcadeDrive(bool reverse) {
     }
 
     // Applies the speeds to the drivetrain
-    //drivetrain->arcade(fwdSpeed, rotSpeed);
-    drivetrain->arcade(leftY, rightX);
+    drivetrain->arcade(fwdSpeed, rotSpeed);
 }
 
 /** 
@@ -106,89 +101,119 @@ void setDriveMotorCurrentLimits(int mAmps) {
 }
 
 /**
-* @brief Turns the robot to an absolute heading using PID control
+* @brief Turn the robot to an absolute heading using PID control
 * @param heading The heading to turn to in degrees: [0,360)
-* @param timeout Timeout before the robot gives up in seconds, default to 10
 * @param maxVelocity The maximum velocity of the robot in the range [0,1], default to 1
+* @param timeout Timeout before the robot gives up in milliseconds, default to 5000
+* @param behavior The behavior of the turn, LEFT, RIGHT, or DEFAULT (closest direction), default to DEFAULT
 */
-void turnToHeading(double heading, double maxVelocity, int timeout) {
+void turnToHeading(double heading, double maxVelocity, int timeout, enum TurnBehavior behavior) {
+    // Get the current heading
 	double currentHeading = gyro.get_heading();
 	double error = heading - currentHeading;
 	
-	if (error > 180) {
-		error = error - 360;
-	} else if (error < -180) {
-		error = 360 + error;
-	}
+    if (behavior == TurnBehavior::DEFAULT) {
+        // Normalize the error
+        if (error > 180) {
+            error = error - 360;
+        } else if (error < -180) {
+            error = 360 + error;
+        }
+    } else if (behavior == TurnBehavior::LEFT) {
+        if (error > 0) {
+            error = -360 + error;
+        }
+    } else if (behavior == TurnBehavior::RIGHT) {
+        if (error < 0) {
+            error = 360 + error;
+        }
+    }
+
 	turnAngle(error, maxVelocity, timeout);
 }
 
 /** 
-* @brief Custom Turnangle Function
+* @brief Turn the robot by the specified angle using PID control
 * @param angle angle in degrees
-* @param timeout timeout before the robot gives up in seconds
+* @param maxVelocity maximum velocity of the robot in the range [0,1], default to 1
+* @param timeout timeout before the robot gives up in milliseconds, default to 5000
 */
-
 void turnAngle(double angle, double maxVelocity, int timeout) {
-    auto gains = get<1>(chassis->getGains());
+    // Calculate the target angle
     double target = angle + gyro.get_rotation();
     double error = angle;
 	double previousError = 0;
 	double integral = 0;
-    double smallErrorRange = 0.4;
-    double smallErrorTimeout = 100;
-    double largeErrorRange = 0.75;
-    double largeErrorTimeout = 500;
-    double minVelocity = 3.0;
+
+    // Set the start time and exit time
 	int startTime = pros::millis();
-	auto exitTime = std::chrono::high_resolution_clock::now() + std::chrono::seconds(timeout);
+	int exitTime = pros::millis() + timeout;
+
+    int smallErrorEntryTime = -1; // Time when the robot entered the small error range
+    int largeErrorEntryTime = -1; // Time when the robot entered the large error range
+
+    // Loop until the target is reached or the timeout is reached
     bool targetReached = false;
-    double smallErrorEntryTime = 0;
-    double largeErrorEntryTime = 0;
-	while (!targetReached && std::chrono::high_resolution_clock::now() < exitTime) {
-		integral = integral * 0.8 + error;
-		double velocity = gains.kP * error + (error - previousError) * gains.kD + gains.kI * integral;
+    int currentTime = pros::millis();
+    int previousTime = currentTime;
+	while (!targetReached && currentTime < exitTime) {
+        int timeChange = currentTime - previousTime;
+
+        // Calculate the velocity
+		double velocity = turnPID.kP * error + ((error - previousError) * turnPID.kD / timeChange) + (turnPID.kI * integral * timeChange);
         if (velocity > 0) {
-            velocity = std::clamp(velocity+minVelocity, 0.0, 600.0*maxVelocity);
+            velocity = std::clamp(velocity+turnPID.minVelocity, 0.0, 600.0*maxVelocity);
         } else if (velocity < 0) {
-            velocity = std::clamp(velocity-minVelocity, -600.0*maxVelocity, 0.0);
+            velocity = std::clamp(velocity-turnPID.minVelocity, -600.0*maxVelocity, 0.0);
         }
+
+        // Set the motor velocities
 		rightMotorGroup.moveVelocity(-velocity);
 		leftMotorGroup.moveVelocity(velocity);
 		pros::delay(5);
-        if (abs(error) < smallErrorRange) {
-            if (smallErrorEntryTime == 0) {
+
+        // Determine if within small error range
+        if (abs(error) < turnPID.smallErrorRange) {
+            // Set the entry time if not already set
+            if (smallErrorEntryTime == -1) {
                 smallErrorEntryTime = pros::millis();
             }
-            if (smallErrorEntryTime + smallErrorTimeout < pros::millis()) {
-                print(2, 0, "Small Error");
+            // Check if the timeout has been reached
+            if (smallErrorEntryTime + turnPID.smallErrorTimeout < pros::millis()) {
                 targetReached = true;
                 break;
             }
         } else {
-            smallErrorEntryTime = 0;
+            // Reset the entry time
+            smallErrorEntryTime = -1;
         }
-        if (abs(error) < largeErrorRange) {
-            if (largeErrorEntryTime == 0) {
+        // Determine if within large error range
+        if (abs(error) < turnPID.largeErrorRange) {
+            // Set the entry time if not already set
+            if (largeErrorEntryTime == -1) {
                 largeErrorEntryTime = pros::millis();
             }
-            if (largeErrorEntryTime + largeErrorTimeout < pros::millis()) {
-                print(2, 0, "Large Error");
+            // Check if the timeout has been reached
+            if (largeErrorEntryTime + turnPID.largeErrorTimeout < pros::millis()) {
                 targetReached = true;
                 break;
             }
         } else {
-            largeErrorEntryTime = 0;
+            // Reset the entry time
+            largeErrorEntryTime = -1;
         }
+
+        // Update the error values
 		previousError = error;
 		error = target - gyro.get_rotation();
+        integral = integral * 0.8 + error;
+
+        // Update the time values
+        previousTime = currentTime;
+        currentTime = pros::millis();
 	}
     drivetrain->stop();
-    print(0, 0, "Heading: " + std::to_string(gyro.get_heading()));
-    if (!targetReached) {
-        print(2, 0, "Timeout");
-    }
-    pros::delay(5000);
+    pros::delay(50);
 }
 
 void moveDistanceWithTimeout(QLength itarget, int timeout) {
